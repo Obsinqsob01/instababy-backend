21ec3608654d70b90605d4575a7f5ec959c8fe61
Luis Contreras Guerra <luis@mailvibes.io>
2020-12-01 21:45:16 -0600

    Create controllers to add and delete comment

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,9 +1,10 @@
-const { PostService } = require('../services');
+const { PostService } = require("../services");
 
 module.exports = {
   create: async (req, res) => {
     try {
-      const { body } = req;
+      const { decoded, body } = req;
+      body.user_id = decoded.id;
 
       const post = await PostService.create(body);
 
@@ -12,6 +13,38 @@ module.exports = {
       });
     } catch (error) {
       return res.status(400).send({
+        message: error.message,
+      });
+    }
+  },
+  createComment: async (req, res) => {
+    try {
+      const { decoded, body } = req;
+      const { id } = req.params;
+
+      const comment = await PostService.createComment(id, decoded.id, body);
+
+      res.status(201).send({
+        comment,
+      });
+    } catch (error) {
+      res.status(400).send({
+        message: error.message,
+      });
+    }
+  },
+  removeComment: async (req, res) => {
+    try {
+      const { decoded, body } = req;
+      const { id } = req.params;
+
+      const comment = await PostService.removeComment(id, body.comment_id);
+
+      res.status(200).send({
+        comment,
+      });
+    } catch (error) {
+      res.status(400).send({
         message: error.message,
       });
     }
