0dfae52497d5525230642ee8c1f8be93e9a81878
Luis Contreras Guerra <luis@mailvibes.io>
2020-12-01 21:45:53 -0600

    create service to add and delete a comment

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,5 +1,9 @@
-const { Post } = require('../models');
+const { Post } = require("../models");
 
 module.exports = {
   create: (body) => new Post(body).save(),
+  createComment: (id, user_id, { comment }) =>
+    Post.findByIdAndUpdate(id, { $push: { comments: { user_id, comment } } }),
+  removeComment: (id, comment_id) =>
+    Post.findByIdAndUpdate(id, { $pull: { comments: { _id: comment_id } } }),
 };
