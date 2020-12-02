dd94e2605bac61ed0310ab755fa40468842a6935
Luis Contreras Guerra <luis@mailvibes.io>
2020-12-01 21:45:30 -0600

    Add routes to add and delete a comment

--- src/routes/PostRouter.js
+++ src/routes/PostRouter.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const { verify } = require("jsonwebtoken");
 const PostController = require("../controllers/PostController");
+const { verifyToken } = require("../middlewares");
+const { PostValidator } = require("../validators");
 
 const router = express.Router();
 
-router.post("/posts", PostController.create);
+router.post("/posts", verifyToken, PostValidator.create, PostController.create);
+router.patch(
+  "/posts/:id/comment",
+  verifyToken,
+  PostValidator.createComment,
+  PostController.createComment
+);
+router.delete(
+  "/posts/:id/comment",
+  PostValidator.removeComment,
+  PostController.removeComment
+);
 
 module.exports = router;
