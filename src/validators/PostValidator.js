870d2db6679c7b8005d1feaca61e4a3b041bd83c
Luis Contreras Guerra <luis@mailvibes.io>
2020-12-01 21:46:05 -0600

    Create posts validators

---
+++ src/validators/PostValidator.js
@@ -0,0 +1,26 @@
+const { celebrate, Segments, Joi } = require("celebrate");
+
+module.exports = {
+  create: celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      description: Joi.string(),
+      img_url: Joi.string().uri().required(),
+    }),
+  }),
+  createComment: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+      comment: Joi.string().required(),
+    }),
+  }),
+  removeComment: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+      comment_id: Joi.string().required(),
+    }),
+  }),
+};
