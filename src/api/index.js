e6b9a5fc1c8ec6d1ad170903cf4d824191d991f8
Luis Contreras Guerra <luis@mailvibes.io>
2020-12-07 20:16:08 -0600

    Using cors

--- src/api/index.js
+++ src/api/index.js
@@ -3,11 +3,13 @@ const routes = require("../routes");
 const morgan = require("morgan");
 const celebrate = require("celebrate");
 const { dateNow } = require("../middlewares");
+import cors from "cors";
 
 const PORT = process.env.PORT || 3006;
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(dateNow);
